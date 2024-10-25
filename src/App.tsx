974d98e87ffc71144c6c0636bc57ea349368c03a
userasl0007 <userasl0007@gmail.com>
2024-10-25 12:26:50 +0530

    1.1

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reuyhyiuhu9act
         </a>
       </header>
     </div>
